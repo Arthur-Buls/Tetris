1f91f9dc019750407a7c8f28728e12122c3bcd72
Arthur-Buls <happyaxel94@gmail.com>
2016-12-11 12:35:42 +0200

    Adding main components
    Modifications
    --------------
    * Renamed form and files
    * Added canvas to the project with demo action

---
+++ Tetris/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void MainPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            var graphics = MainPictureBox.CreateGraphics();
+
+            graphics.DrawRectangle(Pens.Red, new Rectangle(e.X-50, e.Y-50, 100, 100));
+        }
+    }
+}
