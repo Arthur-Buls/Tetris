a3bfea75e8a4a5975cf155a86235ca349f251280
Arthur-Buls <happyaxel94@gmail.com>
2016-12-13 20:22:49 +0200

    Create interface hierarchy
    Modifications
    ----------------
    * Created basic interfaces

---
+++ Tetris/Interfaces/IGameField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris.Interfaces.Objects;
+
+namespace Tetris.Interfaces
+{
+    public interface IGameField
+    {
+        Graphics NextFigureGraphics { get; set; }
+        Graphics MainGraphics { get; set; }
+        IList<IObject> Objects { get; set; }
+        IObject NextObject { get; set; }
+
+        IDrawable FieldGrid { get; set; }
+        
+        /// <summary>
+        /// Display all objects on Main Graphics
+        /// </summary>
+        void Draw();
+
+        /// <summary>
+        /// React on timer tick
+        /// </summary>
+        void Tick();
+    }
+}
---
+++ Tetris/Interfaces/Objects/IObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Interfaces.Objects
+{
+    public interface IObject : IDrawable
+    {
+        bool IsVisible { get; set; }
+        ICoordinates Coordinates { get; set; }
+        IGameField GameField { get; set; }
+
+        /// <summary>
+        /// Move to particular coordinates
+        /// </summary>
+        /// <param name="offset">Exact coordinates to move on</param>
+        /// <returns></returns>
+        ICoordinates Move(ICoordinates newCoordinates);
+
+        /// <summary>
+        /// Shift object on exact offset
+        /// </summary>
+        /// <param name="offset">Offset</param>
+        /// <returns>null if shift is completed, existing object if it</returns>
+        bool Shift(ICoordinates offset);
+    }
+}
