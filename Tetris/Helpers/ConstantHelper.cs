b6273136e3aeb74a816f29339ff63f3f054c7a37
Денис <denlik94@gmail.com>
2016-12-18 11:24:38 +0000

    GameGridDrawing Modifications ----------- * Game Field Class implementation * Add ConstantHelper

---
+++ Tetris/Helpers/ConstantHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Tetris.Helpers
+{
+    public static class ConstantHelper
+    {
+        public static int BlockXNumber
+        {
+            get { return 20; }
+        }
+        public static int BlockYNumber
+        {
+            get { return 40; }
+        }
+        public static Pen GridPen = new Pen(Brushes.HotPink, 1);
+    }
+}
